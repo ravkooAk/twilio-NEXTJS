537d4afab7fe9f3d8399b16e973a99fe9231d7e0
akashkoco <91880623+akashkoco@users.noreply.github.com>
2022-12-27 20:39:49 +0530

    fix: code for the otp

--- pages/api/send-otp.ts
+++ pages/api/send-otp.ts
@@ -11,35 +11,36 @@ async function sendOTP(phone: string) {
   try {
     // const otp = Math.floor(1000 + Math.random() * 9000);  // Generate a random 4-digit OTP
 
-    await client.messages.create({
-      to: `+91${phone}`,
-      from: process.env.TWILIO_PHONE_NUMBER
-    });
-  } catch (error) {
-    console.error(error);
-    throw new Error('Failed to send OTP');
-  }
+//     await client.messages.create({
+//       to: `+91${phone}`,
+//       from: process.env.TWILIO_PHONE_NUMBER
+//     });
+//   } catch (error) {
+//     console.error(error);
+//     throw new Error('Failed to send OTP');
+//   }
 
+// }
+// }
+
+  const accountSid = process.env.TWILIO_ACCOUNT_SID;
+  const authToken = process.env.TWILIO_AUTH_TOKEN;
+  const client = twilio(accountSid, authToken);
+  const servicesId = process.env.TWILIO_SERVICE_SID;
+
+  // verify the otp code
+    client.verify
+    .services(servicesId)
+    .verifications.create({
+        to: req.body.phone,
+        channel: "sms",
+    })
+    .then((data) => {
+        res.status(200).json({ message: "OTP sent successfully" });
+    }
+    )
+    .catch((err) => {
+        res.status(500).json({ message: "Error sending OTP" });
+    }
+    );
 }
-}
-
-//   const accountSid = process.env.TWILIO_ACCOUNT_SID;
-//   const authToken = process.env.TWILIO_AUTH_TOKEN;
-//   const client = twilio(accountSid, authToken);
-//   const servicesId = process.env.TWILIO_SERVICE_SID;
-
-//   // verify the otp code
-//     client.verify
-//     .services(servicesId)
-//     .verifications.create({
-//         to: req.body.phone,
-//         channel: "sms",
-//     })
-//     .then((data) => {
-//         res.status(200).json({ message: "OTP sent successfully" });
-//     }
-//     )
-//     .catch((err) => {
-//         res.status(500).json({ message: "Error sending OTP" });
-//     }
-//     );
